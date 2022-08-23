d1337bfcee610695eca6bdb14bb535971b53a485
waka0905 <priminister@icloud.com>
2022-08-23 18:29:46 +0900

    サイト作成

--- database/migrations/2022_08_01_151926_create_sytes_table.php
+++ database/migrations/2022_08_01_151926_create_sytes_table.php
@@ -20,6 +20,17 @@ class CreateSytesTable extends Migration
             $table->string('ganban', 50);
             $table->string('tansan', 50);
             $table->string('restaurant', 50);
+            $table->timestamps();
+            $table->string('url', 2083)->nullable();
+            $table->string('prefecture', 100)->nullable();
+            $table->string('address', 100)->nullable();
+            $table->string('nearest', 100)->nullable();
+            $table->string('price', 100)->nullable();
+            $table->string('time', 100)->nullable();
+            $table->string('close', 100)->nullable();
+            $table->string('bus', 100)->nullable();
+            $table->string('direction', 100)->nullable();
+            
         });
     }
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(SytesTableSeeder::class);
-        $this->call(PicturesTableSeeder::class);
     }
 }
